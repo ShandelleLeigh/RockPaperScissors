5c87eae09b82051f15dced131803f4db2711e7ca
Shandelle Lovato <ShandelleLovato@gmail.com>
2018-02-14 17:18:20 -0700

    sets up git, with my progress so far

--- rps.js
+++ rps.js
@@ -19,76 +19,66 @@
 //once winner is determined, there is an element that shows: you win or lose
 
 
+//Declare variables
 
-function compyChoice() {
-  var userChoice = ['compyRock', 'compyPaper', 'compyScissors'];
-  var index = Math.floor(Math.random() * 3);
-  return userChoice[index];
-  // console.log(choices[index])
-}
 
-var compChoice = compyChoice();
-console.log(compChoice);
 
-function evalUserChoice() {
-  switch (true) {
-    case (userChoice == 'userRock' && compChoice == 'compyRock'):
-      alert('Tie');
-      return Tie;
-      break;
-    case (userChoice == 'userRock' && compChoice == 'compyPaper'):
-      return Loss;
-      alert('Loss');
-      break;
-    case (userChoice == 'userRock' && compChoice == 'compyScissors'):
-      return Win;
-      alert('Win');
-      break;
-    case (userChoice == 'userPaper' && compChoice == 'compyRock'):
-      return Win;
-      alert('Win');
-      break;
-    case (userChoice == 'userPaper' && compChoice == 'compyPaper'):
-      alert('Tie');
-      return Tie;
-      break;
-    case (userChoice == 'userPaper' && compChoice == 'compyScissors'):
-      return Loss;
-      alert('Loss');
-      break;
-    case (userChoice == 'userScissors' && compChoice == 'compyRock'):
-      return Loss;
-      alert('Loss');
-      break;
-    case (userChoice == 'userScissors' && compChoice == 'compyPaper'):
-      return Win;
-      alert('Win');
-      break;
-    case (userChoice == 'userScissors' && compChoice == 'compyScissors'):
-      return Tie;
-      alert('Tie');
-      break;
-    default:
-      console.log('error in evalUserChoice');
-      break;
+//Functions
+
+// $(document).ready( function() {
+
+  var options = ['rock', 'paper', 'scissors'];
+  var compyChoice = computerChoice;
+  var user = []
+
+
+  // function computerChoice() {
+  //   var index = Math.floor(Math.random() * options.length)
+  //   return options[index];
+  //   console.log(compyChoice);
+  // }
+  // console.log(compyChoice);
+
+
+
+
+  function computerChoice() {
+    var index = Math.floor(Math.random() * 3)
+    console.log(index);
+    console.log(options[index]);
+    return options[index];
+    // console.log(coptions[index]);
+    // console.log(computerChoice);
   }
-}
 
 
-function startGame(e) {
-  var choice = e.target.id;
-  console.log(choice);
-  compyChoice();
-  evalUserChoice();
 
-}
 
-var userChoice = document.getElementsByClassName('choice')
-for (var i in userChoice) {
-  try {
-    userChoice[i].addEventListener('click', startGame)
 
-  } catch(err) {
-    console.log("shit's borked")
+
+  function compareChoices() {}
+
+  function displayResult() {}
+
+  function calculateTallies() {}
+
+  function recordResults() {}
+
+  function startGame(e) {
+    user = e.target.id;
+    stats[user]++
+    computerChoice();
+    compare();
+    printResults();
+    calcTotals();
+    // console.log(compyChoice);
   }
-}
+
+  $('.choice').on('click',startGame, function() {
+    var userChoice = $(this).attr("value")
+    computerChoice();
+    console.log(userChoice);
+    console.log(user);
+  })
+
+// })
